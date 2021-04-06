a53d01f4bf799df982ce798c4eddd444e60accbc
Din Avrunin <dobzik@hotmail.com>
2021-04-06 14:02:19 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <EventCard v-for="event in events" :key ="event.id" :event="event" />
+  <!-- <EventCard/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '../components/EventCard.vue';
+
+export default {
+  name: "Home",
+    data(){
+    return{
+      events:[
+        {
+          id: 1,
+          date: "Feburary 20",
+          time: "13:30"
+        },
+        {      
+          id: 2,
+          date: "januar 15",
+          time: "15:30"
+        },
+        {
+          id: 3,
+          date: "june 12",
+          time: "17:24"
+        }
+      ]}},
+  components: {
+    EventCard
+  },
+  
+};
+</script>
